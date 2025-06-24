9658ef1ace20c3d626545efa9b8a2375a626bfc8
PaoDerass <paola_deras@unitec.edu>
2025-06-23 20:52:47 -0600

    pages.tsx

--- backend/conexion/database.js
+++ backend/conexion/database.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    'localhost',  
+    'examenii',  
     'root',          
-    '',   
+    'root123',   
     {
         host: '127.0.0.1',
         port: 3306,
